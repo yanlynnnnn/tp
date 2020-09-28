80f076424bd5e8aef377b9f56c6550271f6f1563
RuiFengg <ruifeng.chang98@hotmail.com>
2020-09-28 15:27:02 +0800

    Create description class

--- src/main/java/seedu/address/model/util/attributes/Description.java
+++ src/main/java/seedu/address/model/util/attributes/Description.java
@@ -1,4 +1,33 @@
 package seedu.address.model.util.attributes;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.AppUtil;
+
 public class Description {
+
+    public final String value;
+
+    public Description(String description) {
+        requireNonNull(description);
+        value = description;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Description // instanceof handles nulls
+                && value.equals(((Description) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
 }
