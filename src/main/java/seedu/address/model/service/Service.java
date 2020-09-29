6a77a5473f6a777fc54541c7b3a24aba7ce68523
hansebastian <sebastian_hans@hotmail.com>
2020-09-29 18:25:35 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/service/Service.java
+++ src/main/java/seedu/address/model/service/Service.java
@@ -1,5 +1,24 @@
 package seedu.address.model.service;
 
-public class Service {
+import seedu.address.model.util.attributes.Amount;
+import seedu.address.model.util.attributes.Description;
 
+public class Service {
+    /** What type of Service it is */
+    private Description description;
+
+    /** Amount of money in dollars that this Service brings in */
+    private Amount price;
+
+    /** Unique identification number for each Service */
+    private ServiceCode serviceCode;
+
+    /** How long the service takes */
+    private Duration duration;
+
+    Service(Description description, Amount price, Duration duration) {
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+    }
 }
