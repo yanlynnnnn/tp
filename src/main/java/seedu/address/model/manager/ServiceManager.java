aa1276a2cdcc7dfc3c3b4af80c8de80133a55233
hansebastian <sebastian_hans@hotmail.com>
2020-10-05 21:56:32 +0800

    Add UniqueList class
    A UniqueList represents a list that contains unique items. Similar to
    AB3's ClientList, however UniqueList utilizes Java generics for it to be
    extensible to further functionalities that also requires a UniqueList.

--- src/main/java/seedu/address/model/manager/ServiceManager.java
+++ src/main/java/seedu/address/model/manager/ServiceManager.java
@@ -1,4 +1,9 @@
 package seedu.address.model.manager;
 
+/**
+ * Wraps all data at the ServiceManager level
+ * Duplicates are not allowed (by .equals comparison)
+ */
 public class ServiceManager {
+
 }
---
+++ src/main/java/seedu/address/model/service/uniquelist/exceptions/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.service.uniquelist.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified client.
+ */
+public class ItemNotFoundException extends RuntimeException {
+}
