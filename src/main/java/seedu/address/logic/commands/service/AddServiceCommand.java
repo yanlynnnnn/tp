cfe462a99cf3e203a63460f574897398c653a4de
hansebastian <sebastian_hans@hotmail.com>
2020-10-05 16:29:34 +0800

    Add prefixes for service to CliSyntax

---
+++ src/main/java/seedu/address/logic/commands/service/AddServiceCommand.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands.service;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Adds a service to SuperSalon.
+ */
+public class AddServiceCommand extends Command {
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/service/AddServiceCommandParser.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.parser.service;
+
+import seedu.address.logic.commands.service.AddServiceCommand;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+
+/**
+ * Parses input arguments and creates a new AddServiceCommand object.
+ */
+public class AddServiceCommandParser implements Parser<AddServiceCommand> {
+
+    @Override
+    public AddServiceCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
