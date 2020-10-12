9982d25d3ce88a12b616e0309187df429b5dfded
hansebastian <sebastian_hans@hotmail.com>
2020-10-12 13:08:05 +0800

    Edit find service command parser to take prefix

--- src/main/java/seedu/address/logic/parser/service/FindServiceCommandParser.java
+++ src/main/java/seedu/address/logic/parser/service/FindServiceCommandParser.java
@@ -1,12 +1,32 @@
 package seedu.address.logic.parser.service;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ISFIXED;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SERVICE_SERVICE_CODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SERVICE_TITLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+import seedu.address.logic.commands.expense.FindExpenseCommand;
 import seedu.address.logic.commands.service.FindServiceCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.expense.DatePredicate;
+import seedu.address.model.expense.DescriptionPredicate;
+import seedu.address.model.expense.Expense;
+import seedu.address.model.expense.IsFixedPredicate;
+import seedu.address.model.expense.TagPredicate;
+import seedu.address.model.service.Service;
 import seedu.address.model.service.ServiceContainKeywordPredicate;
 
 /**
@@ -14,20 +34,42 @@ import seedu.address.model.service.ServiceContainKeywordPredicate;
  */
 public class FindServiceCommandParser implements Parser<FindServiceCommand> {
 
+    public static final String MULTIPLE_PARAMETERS = "Please only input one parameter.";
+    public static final int NUM_ALLOWED_PARAMETERS = 1;
+
     /**
      * Parses the given {@code String} of arguments in the context of the FindServiceCommand
      * and returns a FindServiceCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindServiceCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindServiceCommand.MESSAGE_USAGE));
+        // Find better way to check if there is more than one parameter input.
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+            ArgumentTokenizer.tokenize(args, PREFIX_SERVICE_TITLE, PREFIX_SERVICE_SERVICE_CODE);
+
+        if (areMultipleParametersPresent(argMultimap, PREFIX_SERVICE_TITLE, PREFIX_SERVICE_SERVICE_CODE)) {
+            throw new ParseException(MULTIPLE_PARAMETERS);
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        Predicate<Service> predicate = null;
+        if (argMultimap.getValue(PREFIX_SERVICE_TITLE).isPresent()) {
+            predicate = new ServiceTitlePredicate(ParserUtil.parseDescription(
+                argMultimap.getValue(PREFIX_SERVICE_TITLE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_SERVICE_SERVICE_CODE).isPresent()) {
+            predicate = new ServiceCodePredicate(ParserUtil.parseIsFixed(argMultimap.getValue(PREFIX_SERVICE_SERVICE_CODE)
+                .get()));
+        }
 
-        return new FindServiceCommand(new ServiceContainKeywordPredicate(Arrays.asList(nameKeywords)));
+        return new FindServiceCommand(predicate);
+    }
+
+    /**
+     * Returns true if there is more than one input parameters.
+     */
+    private static boolean areMultipleParametersPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).filter(prefix -> argumentMultimap.getValue(prefix).isPresent()).count()
+            > NUM_ALLOWED_PARAMETERS;
     }
 }
