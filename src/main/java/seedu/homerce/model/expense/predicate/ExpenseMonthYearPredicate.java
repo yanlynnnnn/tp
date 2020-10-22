66b8c610bcb8896616047904f13c4d129c4ad218
Tan Yan Lyn <e0424594@u.nus.edu>
2020-10-22 14:51:04 +0800

    Implement BreakdownFinance

---
+++ src/main/java/seedu/homerce/model/expense/predicate/ExpenseMonthYearPredicate.java
@@ -0,0 +1,35 @@
+package seedu.homerce.model.expense.predicate;
+
+import java.time.Month;
+import java.time.Year;
+import java.util.function.Predicate;
+
+import seedu.homerce.model.expense.Expense;
+
+/**
+ * Tests that a {@code Expense}'s {@code Date} matches the given month and year.
+ */
+public class ExpenseMonthYearPredicate implements Predicate<Expense> {
+    private final Month month;
+    private final Year year;
+
+    public ExpenseMonthYearPredicate(Month month, Year year) {
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public boolean test(Expense expense) {
+        Month expenseMonth = expense.getDate().getMonth();
+        int expenseYear = expense.getDate().getYear();
+        return month.equals(expenseMonth) && year.getValue() == expenseYear;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ExpenseMonthYearPredicate // instanceof handles nulls
+                && month.equals(((ExpenseMonthYearPredicate) other).month)
+                && year.equals(((ExpenseMonthYearPredicate) other).year)); // state check
+    }
+}
