bd06148e23bd87ac60adae99c3c44e2e4cd53059
hansebastian <sebastian_hans@hotmail.com>
2020-10-28 02:14:22 +0800

    Fix CI errors

--- src/test/java/seedu/homerce/model/manager/HistoryManagerTest.java
+++ src/test/java/seedu/homerce/model/manager/HistoryManagerTest.java
@@ -3,44 +3,41 @@ package seedu.homerce.model.manager;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.homerce.testutil.service.TypicalServices.MANICURE;
 
-import java.time.Month;
-import java.time.Year;
-
 import org.junit.jupiter.api.Test;
 
-import seedu.homerce.logic.commands.BreakdownFinanceCommand;
-import seedu.homerce.logic.commands.schedule.CustomWeekCommand;
+import seedu.homerce.commons.core.index.Index;
 import seedu.homerce.logic.commands.service.AddServiceCommand;
-import seedu.homerce.logic.commands.service.ListServiceCommand;
+import seedu.homerce.logic.commands.service.DeleteServiceCommand;
 import seedu.homerce.model.Model;
 import seedu.homerce.model.ModelManager;
-import seedu.homerce.model.util.attributes.Date;
 
 public class HistoryManagerTest {
 
     @Test
-    public void addToHistory() {
+    public void addToHistory_stateChange_success() {
+        Model modelTest = new ModelManager();
+        HistoryManager historyTest = HistoryManager.getInstance();
+        AddServiceCommand addServiceCommandTest = new AddServiceCommand(MANICURE);
+
+        historyTest.addToHistory(modelTest, addServiceCommandTest);
+        assertTrue(historyTest.getPreviousHistory().getModel().getServiceManager().getServiceList().isEmpty());
+
+        historyTest.addToHistory(modelTest, addServiceCommandTest);
+        assertTrue(historyTest.getPreviousHistory().getModel().getServiceManager().getServiceList().isEmpty());
+    }
+
+    @Test
+    public void getPreviousHistory_stateChange_returnsPreviousHistory() {
         Model modelTest = new ModelManager();
         HistoryManager historyTest = HistoryManager.getInstance();
         AddServiceCommand commandTest = new AddServiceCommand(MANICURE);
-        ListServiceCommand listServiceCommandTest = new ListServiceCommand();
 
         historyTest.addToHistory(modelTest, commandTest);
-
         assertTrue(historyTest.getPreviousHistory().getCommand().equals(commandTest));
 
-        // List command will not be added to history -> throw exception when poll
-        historyTest.addToHistory(modelTest, listServiceCommandTest);
-
-        BreakdownFinanceCommand breakdownFinanceCommand = new BreakdownFinanceCommand(Month.of(12),
-            Year.of(2020));
-        assertTrue(historyTest.getPreviousHistory() == null);
-
-        historyTest.addToHistory(modelTest, breakdownFinanceCommand);
-        assertTrue(historyTest.getPreviousHistory() == null);
-
-        CustomWeekCommand customWeekCommandTest = new CustomWeekCommand(new Date("02-02-2020"));
-        historyTest.addToHistory(modelTest, customWeekCommandTest);
-        assertTrue(historyTest.getPreviousHistory() == null);
+        DeleteServiceCommand deleteServiceCommandTest = new DeleteServiceCommand(Index.fromZeroBased(1));
+        historyTest.addToHistory(modelTest, deleteServiceCommandTest);
+        assertTrue(historyTest.getPreviousHistory().getCommand().equals(deleteServiceCommandTest));
     }
+
 }
