c23c54eee1d5e94e4dfb228933c52d6ff19e9b6f
hansebastian <sebastian_hans@hotmail.com>
2020-10-28 00:52:15 +0800

    Update undo command to account for new commands

---
+++ src/test/java/seedu/homerce/model/manager/HistoryManagerTest.java
@@ -0,0 +1,16 @@
+package seedu.homerce.model.manager;
+
+import org.junit.jupiter.api.Test;
+
+public class HistoryManagerTest {
+
+    @Test
+    public void getModel() {
+
+    }
+
+    @Test
+    public void getCommand() {
+
+    }
+}
