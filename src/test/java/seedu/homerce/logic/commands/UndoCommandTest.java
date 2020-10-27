0ee69e68b15362c261e4e3d96d0737e092dae6e9
hansebastian <sebastian_hans@hotmail.com>
2020-10-28 01:43:32 +0800

    Add tests for undo feature

--- src/test/java/seedu/homerce/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/homerce/logic/commands/UndoCommandTest.java
@@ -1,59 +1,21 @@
 package seedu.homerce.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.ArrayList;
+import static seedu.homerce.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.homerce.logic.commands.client.AddClientCommandTest;
-import seedu.homerce.model.manager.ReadOnlyRevenueTracker;
-import seedu.homerce.model.manager.RevenueTracker;
-import seedu.homerce.model.revenue.Revenue;
+import seedu.homerce.logic.commands.exceptions.CommandException;
+import seedu.homerce.model.Model;
+import seedu.homerce.model.ModelManager;
+import seedu.homerce.model.manager.HistoryManager;
 
 public class UndoCommandTest {
-
-    @Test
-    public void constructor_nullRevenue_throwsNullPointerException() {
-
-    }
-
-    @Test
-    public void constructor_nullModel_throwsNullPointerException() {
-
-    }
-
     @Test
     public void execute_latestChange_throwsCommandException() {
+        HistoryManager historyTest = HistoryManager.getInstance();
+        Model modelTest = new ModelManager();
 
-    }
-
-    @Test
-    public void execute_validUndo_successfulUndo() {
-
-    }
-
-    @Test
-    public void equals() {
-
-    }
-
-    /**
-     * A Model stub that always accepts the expense being added.
-     */
-    private class ModelStubAcceptingRevenueAdded extends AddClientCommandTest.ModelStub {
-
-        final ArrayList<Revenue> revenuesAdded = new ArrayList<>();
-
-        @Override
-        public void addRevenue(Revenue revenue) {
-            requireNonNull(revenue);
-            revenuesAdded.add(revenue);
-        }
-
-        @Override
-        public ReadOnlyRevenueTracker getRevenueTracker() {
-            return new RevenueTracker();
-        }
+        UndoCommand undoTest = new UndoCommand();
+        assertThrows(CommandException.class, () -> undoTest.execute(modelTest, historyTest));
     }
 }
--- src/test/java/seedu/homerce/model/manager/HistoryManagerTest.java
+++ src/test/java/seedu/homerce/model/manager/HistoryManagerTest.java
@@ -1,16 +1,46 @@
 package seedu.homerce.model.manager;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.homerce.testutil.service.TypicalServices.MANICURE;
+
+import java.time.Month;
+import java.time.Year;
+
 import org.junit.jupiter.api.Test;
+
+import seedu.homerce.logic.commands.BreakdownFinanceCommand;
+import seedu.homerce.logic.commands.schedule.CustomWeekCommand;
+import seedu.homerce.logic.commands.service.AddServiceCommand;
+import seedu.homerce.logic.commands.service.ListServiceCommand;
+import seedu.homerce.model.Model;
+import seedu.homerce.model.ModelManager;
+import seedu.homerce.model.util.attributes.Date;
 
 public class HistoryManagerTest {
 
     @Test
-    public void getModel() {
+    public void addToHistory() {
+        Model modelTest = new ModelManager();
+        HistoryManager historyTest = HistoryManager.getInstance();
+        AddServiceCommand commandTest = new AddServiceCommand(MANICURE);
+        ListServiceCommand listServiceCommandTest = new ListServiceCommand();
 
-    }
+        historyTest.addToHistory(modelTest, commandTest);
 
-    @Test
-    public void getCommand() {
+        assertTrue(historyTest.getPreviousHistory().getCommand().equals(commandTest));
 
+        // List command will not be added to history -> throw exception when poll
+        historyTest.addToHistory(modelTest, listServiceCommandTest);
+
+        BreakdownFinanceCommand breakdownFinanceCommand = new BreakdownFinanceCommand(Month.of(12),
+            Year.of(2020));
+
+        historyTest.addToHistory(modelTest, breakdownFinanceCommand);
+        assertThrows(NullPointerException.class, () -> historyTest.getPreviousHistory().getCommand());
+
+        CustomWeekCommand customWeekCommandTest = new CustomWeekCommand(new Date("02-02-2020"));
+        historyTest.addToHistory(modelTest, customWeekCommandTest);
+        assertThrows(NullPointerException.class, () -> historyTest.getPreviousHistory().getCommand());
     }
 }
