81a9253075163bad874aa090b8fea0850a6825ae
Tan Yan Lyn <e0424594@u.nus.edu>
2020-11-01 19:37:52 +0800

    Resize GUI MainWindow and FinanceWindow

--- src/main/java/seedu/homerce/ui/financialpanel/FinanceWindow.java
+++ src/main/java/seedu/homerce/ui/financialpanel/FinanceWindow.java
@@ -61,8 +61,8 @@ public class FinanceWindow extends UiPart<Stage> {
      * Sets the default size based on {@code guiSettings}.
      */
     public void setWindowDefaultSize(GuiSettings guiSettings) {
-        this.getRoot().setHeight(guiSettings.getWindowHeight() * 0.85);
-        this.getRoot().setWidth(guiSettings.getWindowWidth() * 0.75);
+        this.getRoot().setHeight(guiSettings.getWindowHeight() * 0.65);
+        this.getRoot().setWidth(guiSettings.getWindowWidth() * 0.85);
         if (guiSettings.getWindowCoordinates() != null) {
             this.getRoot().setX(guiSettings.getWindowCoordinates().getX());
             this.getRoot().setY(guiSettings.getWindowCoordinates().getY());
